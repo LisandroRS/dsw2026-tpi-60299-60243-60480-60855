c74ea74db490f4a574a08fc440431e3573214a2e
kira25-sys <fontsfelipe77@gmail.com>
2026-07-23 16:26:32 -0300

    creacion del dto de especialidad junto con su interfaz

---
+++ Dsw2026Tpi.Application/Dtos/SpecialityModel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Dsw2026Tpi.Application.Dtos
+{
+    public record SpecialityModel
+    {
+        public record Request(string name, string Description);
+        public record Response(Guid id,string name, string Description);
+    }
+
+}
