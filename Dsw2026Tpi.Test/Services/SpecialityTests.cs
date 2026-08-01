d10a38dab59bec274190f921125efc18685465d3
Lautaro Adler <lautaroadler@gmail.com>
2026-08-01 18:04:03 -0300

    Primera prueba unitaria

---
+++ Dsw2026Tpi.Test/Services/SpecialityTests.cs
@@ -0,0 +1,41 @@
+﻿using Dsw2026Tpi.Application.Dtos;
+using Dsw2026Tpi.Application.Services;
+using Dsw2026Tpi.CrossCutting.Exceptions;
+using Dsw2026Tpi.Domain.Entities;
+using Dsw2026Tpi.Domain.Interfaces;
+using NSubstitute;
+using Xunit;
+
+namespace Dsw2026Tpi.Test.Services;
+
+public class SpecialityServiceTests
+{
+    private readonly IPersistence _persistence;
+    private readonly SpecialityService _service;
+
+    public SpecialityServiceTests()
+    {
+        _persistence = Substitute.For<IPersistence>();
+        _service = new SpecialityService(_persistence);
+    }
+
+    [Fact]
+    public async Task Crear_CuandoElNombreEstaVacio_DebeLanzarExcepcionDeValidacion()
+    {
+        var request = new SpecialityModel.Request(
+            "",
+            "Descripcion valida");
+
+        var exception =
+            await Assert.ThrowsAsync<ValidationException>(
+                () => _service.Create(request));
+
+        var detail = Assert.Single(exception.Error.Details);
+
+        Assert.Equal("name", detail.Field);
+        Assert.Equal("required", detail.Issue);
+
+        await _persistence.DidNotReceive()
+            .Add(Arg.Any<Speciality>());
+    }
+}
